4b8373bbee2a9705711029a630b146bc09fc80ec
traviscult <traviscult@gmail.com>
2020-03-13 16:01:06 -0400

    added framework and inital design

---
+++ script.js
@@ -0,0 +1,40 @@
+// Script Code
+
+// set variables
+// gameTimer = 75 * 1000ms
+// timerLossForWrongAnswer = 5 * 1000ms
+
+// check for start button
+
+// start timer - update top left timer
+    // decrese timer by 1 seconds (1000ms)
+// Question Loop
+    // display question and answers
+        // display in HTML ID -  quiz-zone
+
+    // check for answer
+        // an event attached to the 4 answeers
+
+    // Input right or wrong
+
+    // wrong no point, -5 timer
+        // gameTimer - tierLossForWrongAnswer
+    // right +1 point
+
+    // if not end of questions || end of timer go back to question loop
+
+// If player rus out of time, nil points - Fail pop up
+    // Do you wnt to play again? ALERT pop up!
+
+
+// After loop is complete check score
+    // if high score enter user name
+    // if not annouce they suck
+
+    // Wait for input to restart game
+
+
+
+
+
+
